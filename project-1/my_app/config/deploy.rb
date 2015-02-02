9d6da9c3da3c67be636c070d3d8df105b11a91ec
David Carney <david@format.com>
2015-02-02 01:50:43 -0500

    capistrano tweaks

--- project-1/my_app/config/deploy.rb
+++ project-1/my_app/config/deploy.rb
@@ -1,8 +1,10 @@
-# Change these
-server '192.168.33.10', port: 7171, roles: [:web, :app, :db], primary: true
+# server '192.168.33.10', user:'vagrant', port: 22, roles: [:web, :app, :db], primary: true
+
+set :git_strategy, RemoteCacheWithProjectRootStrategy
+set :project_root, 'project-1/my_app'
 
 set :repo_url,        'git@github.com:dfcarney/ece1779.git'
-set :application,     'project-1/my_app'
+set :application,     'my_app'
 set :user,            'vagrant'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
@@ -12,7 +14,6 @@ set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-# set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
@@ -27,6 +28,8 @@ set :puma_init_active_record, false  # Change to true if using ActiveRecord
 # JRuby
 set :rbenv_type, :system # or :system, depends on your rbenv setup
 set :rbenv_ruby, 'jruby-1.7.19'
+# set :rbenv_path, '/usr/local/rbenv'
+set :rbenv_custom_path, '/usr/local/rbenv'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
