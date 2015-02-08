fcd3c9d1d1d767b03a6caf895c1da36e1ae1cb5f
David Carney <david@format.com>
2015-02-08 17:23:44 -0500

    handy Trinidad startup script

---
+++ project-1/my_app/bin/trin.sh
@@ -0,0 +1,1 @@
+JAVA_OPTS=-Djava.awt.headless=true -Xms756m -Xmx756m -Xss128m -Xmn512m -XX:PermSize=512m -XX:MaxPermSize=512m -XX:NewRatio=3 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+CMSClassUnloadingEnabled -XX:+CMSClassUnloadingEnabled trinidad
