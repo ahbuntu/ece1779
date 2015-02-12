8292496b7df1b64f120bdcd6010a800909454ac9
David Carney <david@format.com>
2015-02-11 23:32:58 -0500

    production-level Puma config

--- project-1/my_app/config/puma.rb
+++ project-1/my_app/config/puma.rb
@@ -2,4 +2,9 @@ port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 threads     (ENV["MIN_PUMA_THREADS"] || 0), (ENV["MAX_PUMA_THREADS"] || 16)
 #workers     (ENV["MIN_PUMA_WORKERS"] || 1), (ENV["MAX_PUMA_WORKERS"] || 3)
-preload_app!
+
+pidfile "/var/www/my_app/shared/tmp/pids/puma.pid"
+state_path "var/www/my_app/shared/tmp/puma-state"
+activate_control_app
+
+# preload_app!
