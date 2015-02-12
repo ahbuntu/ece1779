738b0fa3d63c59e0bbf56afcce7ae36f5bb268e8
David Carney <david@format.com>
2015-02-12 13:02:28 -0500

    remove capistrano default settings
    * we override them in production.rb

--- project-1/my_app/config/deploy.rb
+++ project-1/my_app/config/deploy.rb
@@ -5,7 +5,7 @@ set :project_root, 'project-1/my_app'
 
 set :repo_url,        'git@github.com:dfcarney/ece1779.git'
 set :application,     'my_app'
-set :user,            'vagrant'
+# set :user,            'vagrant'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
@@ -14,7 +14,7 @@ set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+# set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
