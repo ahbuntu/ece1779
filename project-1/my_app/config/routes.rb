378c210a974cd901d04b19f8cf00f29af03ccee2
David Carney <david@format.com>
2015-02-16 21:43:58 -0500

    sidekiq monitoring

--- project-1/my_app/config/routes.rb
+++ project-1/my_app/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  # sidekiq monitoring: https://github.com/mperham/sidekiq/wiki/Monitoring
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
