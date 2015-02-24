eb1fab06e4f3adf2711380cd0bbe6130f18d573c
David Carney <david@format.com>
2015-02-24 00:12:16 -0500

    shrink_ratio must be >= 1.0

--- project-1/my_app/app/models/auto_scale.rb
+++ project-1/my_app/app/models/auto_scale.rb
@@ -10,8 +10,12 @@ class AutoScale
       self.errors << "Growth threshold must be > shrink threshold" unless self.grow_cpu_thresh > self.shrink_cpu_thresh
       self.errors << "Growth threshold must be <= 100.0" unless self.grow_cpu_thresh <= 100.0
       self.errors << "Shrink threshold must be >= 0" unless self.shrink_cpu_thresh >= 0.0
+
+      # "(e.g., a ratio of 2 doubles the number of workers)."
       self.errors << "Growth ratio must be >= 1.0" unless self.grow_ratio_thresh >= 1.0
-      self.errors << "Shrink ratio must be <= 1.0" unless self.shrink_ratio_thresh <= 1.0
+
+      # "(e.g., a ratio of 4 shuts down 75% of the current workers)."
+      self.errors << "Shrink ratio must be >= 1.0" unless self.shrink_ratio_thresh >= 1.0 
       self.errors.empty?      
     end
 
