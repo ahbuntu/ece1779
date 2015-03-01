02e268f86e9ec415fab2628ca4cd5fe2a9d0a2c9
David Carney <david@format.com>
2015-03-01 18:15:20 -0500

    fix termination cleanup

--- project-1/my_app/app/workers/drain_queue_and_terminate_worker.rb
+++ project-1/my_app/app/workers/drain_queue_and_terminate_worker.rb
@@ -1,16 +1,19 @@
 class DrainQueueAndTerminateWorker
   include Sidekiq::Worker
+  include AwsBoilerplate
 
   # This expects to be executed on the instance being terminated
   # I.e. the Sidekiq queue is also local.
   #
   # ALSO: it assumes that all alarm deletion and ELB deregistration has already been handled.
   def perform(instance_id, terminate_by)
-    worker = Worker.with_id(instance_id)
-    if worker.nil?
+    instance = self.class.all_instances[instance_id]
+    if !instance.exists?
+      Rails.logger.info "[DrainQueueAndTerminateWorker] instance #{instance_id} does not exist. Bailing."
       return
     elsif Time.now >= terminate_by
-      worker.terminate!
+      Rails.logger.info "[DrainQueueAndTerminateWorker] Terminating instance #{instance.id}"
+      instance.terminate
       return
     end
 
@@ -18,7 +21,8 @@ class DrainQueueAndTerminateWorker
     retry_queue.retry_all
 
     if Sidekiq::Queue.all.map(&:size).sum == 0 # doesn't count scheduled jobs
-      worker.terminate! # no jobs left!
+      Rails.logger.info "[DrainQueueAndTerminateWorker] Terminating instance #{instance.id}"
+      instance.terminate # no jobs left
     else
       DrainQueueAndTerminateWorker.perform_in(30.seconds, instance_id, terminate_by)
     end
