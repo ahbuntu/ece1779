0e8889b0c3746b37205eec6bce6dcd86514e903e
David Carney <david@format.com>
2015-03-15 11:37:45 -0400

    Add Question and Answer models

--- project-2/megaphone/application/models.py
+++ project-2/megaphone/application/models.py
@@ -8,10 +8,24 @@ App Engine datastore models
 
 from google.appengine.ext import ndb
 
-
 class ExampleModel(ndb.Model):
     """Example Model"""
     example_name = ndb.StringProperty(required=True)
     example_description = ndb.TextProperty(required=True)
     added_by = ndb.UserProperty()
     timestamp = ndb.DateTimeProperty(auto_now_add=True)
+
+class Question(ndb.Model):
+    """A User asks a Question"""
+    added_by = ndb.UserProperty()
+    timestamp = ndb.DateTimeProperty(auto_now_add=True)
+    text = ndb.StringProperty(required=True)
+    location = ndb.GeoPtProperty(required=True)
+
+class Answer(ndb.Model):
+    """A User answers a Question"""
+    added_by = ndb.UserProperty()
+    timestamp = ndb.DateTimeProperty(auto_now_add=True)
+    text = ndb.StringProperty(required=True)
+    question = ndb.StructuredProperty(Question)
+
