ae3d0bdf1b298728608dfd6fedc2033757af9306
ahmac <ahmadul.hassan@gmail.com>
2015-03-15 18:12:46 -0400

    listing questions is anon. new question is auth

--- project-2/megaphone/application/urls.py
+++ project-2/megaphone/application/urls.py
@@ -37,7 +37,10 @@ app.add_url_rule('/examples/<int:example_id>/edit', 'edit_example', view_func=vi
 app.add_url_rule('/examples/<int:example_id>/delete', view_func=views.delete_example, methods=['POST'])
 
 # List all questions
-app.add_url_rule('/questions', 'list_questions', view_func=views.list_questions, methods=['GET', 'POST'])
+app.add_url_rule('/questions', 'list_questions', view_func=views.list_questions, methods=['GET'])
+
+# Ask a new question
+app.add_url_rule('/new_question', 'new_question', view_func=views.new_question, methods=['POST'])
 
 # Logout
 app.add_url_rule('/logout', 'authenticate', view_func=views.authenticate, methods=['GET'])
--- project-2/megaphone/application/views.py
+++ project-2/megaphone/application/views.py
@@ -30,7 +30,7 @@ cache = Cache(app)
 
 
 def home():
-    return redirect(url_for('list_examples'))
+    return redirect(url_for('list_questions'))
 
 
 def say_hello(username):
@@ -125,8 +125,16 @@ def warmup():
     return ''
 
 def list_questions():
-    """Lists all questions posted on the site"""
+    """Lists all questions posted on the site - available to anonymous users"""
     questions = Question.all()
+    form = QuestionForm()
+    user = users.get_current_user()
+    login_url = users.create_login_url(url_for('home'))
+    return render_template('list_questions.html', questions=questions, form=form, user=user, login_url=login_url)
+
+@login_required
+def new_question():
+    """Creates a new question"""
     form = QuestionForm()
     if form.validate_on_submit():
         question = Question(
@@ -142,8 +150,6 @@ def list_questions():
         except CapabilityDisabledError:
             flash(u'App Engine Datastore is currently in read-only mode.', 'info')
             return redirect(url_for('list_questions'))
-    return render_template('list_questions.html', questions=questions, form=form)
-
 
 def get_location():
     # TODO: this should be moved to client side at some point
@@ -152,8 +158,8 @@ def get_location():
 def authenticate():
     user = users.get_current_user()
     if user:
-        login_url = users.create_login_url(url_for('list_examples'))
+        login_url = users.create_login_url(url_for('home'))
         logout_url = users.create_logout_url(login_url)
         return redirect(logout_url)
     else:
-        return redirect(url_for('list_examples'))
+        return redirect(url_for('home'))
