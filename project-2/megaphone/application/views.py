38332ab1454f6ac1239c188dac9b47f5bc695d36
David Carney <david@format.com>
2015-04-05 15:48:33 -0400

    debugging

--- project-2/megaphone/application/views.py
+++ project-2/megaphone/application/views.py
@@ -542,26 +542,30 @@ def notify_new_answer(answer):
 
     # Broadcast to the owner of the question, unless they answered it.
     if (question.added_by != answer.added_by):
+        logging.info('[CHANNEL] Sending notifications about new answer: ' + answer.key.id())
+
         channel_id = question_answers_channel_id(question.added_by, question)
-        deferred.defer(channel_send_message, channel_id, message, _countdown=4)
+        deferred.defer(channel_send_message, channel_id, message, _countdown=1)
 
         channel_id = all_user_questions_answers_channel_id(question.added_by)
-        deferred.defer(channel_send_message, channel_id, message, _countdown=5)
+        deferred.defer(channel_send_message, channel_id, message, _countdown=2)
 
         channel_id = user_channel_id(question.added_by)
-        deferred.defer(channel_send_message, channel_id, message, _countdown=6)
+        deferred.defer(channel_send_message, channel_id, message, _countdown=3)
+    else:
+        logging.info('[CHANNEL] Question answered by owner. Skipping notifications.')
 
 
 def channel_send_message(channel_id, message):
     tries = 1
     channel_token = channel.create_channel(channel_id)
-    logging.info('starting channel_send_message')
+    logging.info('[CHANNEL] starting channel_send_message to channel: ' + channel_id)
     message_json = json.dumps(message)
 
     for attempt in range(tries):
         # message = 'this is message number: ' + str(attempt)
         channel.send_message(channel_id, message_json)
-        logging.info('just sent: ' + message_json)
+        logging.info('[CHANNEL] just sent to: + channel_id + '() + message_json + ')')
         logging.info(channel_token)
 
 
